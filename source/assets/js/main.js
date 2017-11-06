6d4dd4581518a1aeb883569d506b8d7d792d9f00
Jamie Boyd <enkizero@gmail.com>
2017-11-06 23:00:17 +0000

    Minor updates

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,6 +1,18 @@
 
-var elem = $('<div>');
 
-var thisShowHider = new ShowHide(elem);
+class ShowHide {
+  constructor(element) {
+    this.element = element;
+  }
+
+  get element() {
+    return this.element;
+  }
+}
+
+
+const shElement = document.createElement('div');
+
+const thisShowHider = new ShowHide(shElement);
 
 console.log(thisShowHider);
